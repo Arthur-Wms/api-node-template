6b22ecd124a313512905e4df8c4e33bad09e5dc0
orlandonetto <orlandonetto11@gmail.com>
2021-05-27 09:38:56 -0300

    fix / in endpoint create

--- src/app/routes/user-router.js
+++ src/app/routes/user-router.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const controller = require('../controllers/user-controller');
 const policy = require('../middlewares/user-policy');
 
-router.post('/', policy.register, controller.register);
+router.post('', policy.register, controller.register);
 router.post('/auth', policy.login, controller.localAuth);
 router.post('/auth/google', policy.googleAuth, controller.googleAuth);
 router.get('/load-session', policy.authenticate, controller.loadSession);
