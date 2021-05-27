f3f45845f40e4693a6adf3b4d80b7cea772fe0b1
orlandonetto <orlandonetto11@gmail.com>
2021-05-27 10:00:25 -0300

    code not found fix error

--- src/utils/codes.js
+++ src/utils/codes.js
@@ -3,7 +3,7 @@ module.exports = Object.freeze({
   ERR: "ERR", // Default Error
   USR_CRE: "USR_CRE", // User created
   USR_BCR: "USR_BCR", // Bad credentials
-  NOT_FOUND: "NOTFOUND", // Bad credentials
+  NOT_FOUND: "NOT_FOUND", // Not found
   NOT_AUTH: "NOT_AUTH", // Not Authorized
   EMA_VLD: "EMA_VLD" // Email Validated
 });
